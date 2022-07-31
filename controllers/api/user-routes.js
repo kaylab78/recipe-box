d28ffcfd4637ce94011a98a4bfdf57395baab390
kaylab78 <kaylabackus78@gmail.com>
2022-07-31 15:22:12 -0500

    Wrote POST, PUT and DELETE routes for User

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -35,3 +35,65 @@ router.get('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// POST /api/users (Create a new user)
+router.post('/', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.create({
+        username: username,
+        password: password
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// PUT /api/users/1 (Updated user with id)
+router.put('/:id', (req, res) => {
+    const userId = req.params.id;
+
+    User.update(req.body, {
+        where: {
+            id: userId
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+    const userId = req.params.id;
+
+    User.destroy({
+        where: {
+            id: userId
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
