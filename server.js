aa9c9c2a8234f90f30cd6f9e5e536f46bf0f88d7
kaylab78 <kaylabackus78@gmail.com>
2022-07-31 15:34:37 -0500

    Got server up and working. Tested User routes in Insomnia.

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// { force: true } makes the tables recreate if there are association changes. See boot camp module 13.3.5
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
